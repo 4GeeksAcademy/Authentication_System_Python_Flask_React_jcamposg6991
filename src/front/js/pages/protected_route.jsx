b1d56f55ce89de625be8ef6f44dc52f612e5082f
Jordan Campos Garro <114543285+jcamposg6991@users.noreply.github.com>
2025-02-18 18:25:41 +0000

    codificacion de css de protected_route

--- src/front/js/pages/protected_route.jsx
+++ src/front/js/pages/protected_route.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import "../../styles/protected_route.css";
 
 export const Protected_route = () => {
 	const { store, actions } = useContext(Context);
@@ -14,34 +15,10 @@ export const Protected_route = () => {
 		}
 	}, []);
 
-	const containerStyle = {
-		maxWidth: "600px",
-		margin: "50px auto",
-		padding: "20px",
-		backgroundColor: "#f8f9fa",
-		borderRadius: "8px",
-		boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-		fontFamily: "'Arial', sans-serif",
-		textAlign: "center"
-	};
-
-	const headingStyle = {
-		fontSize: "20px",
-		color: "#333",
-		fontWeight: "bold",
-		marginBottom: "20px"
-	};
-
-	const emailStyle = {
-		fontSize: "18px",
-		color: "#007bff",
-		fontWeight: "bold"
-	};
-
 	return (
-		<div style={containerStyle}>
-			<h3 style={headingStyle}>Usuario autenticado:</h3>
-			<p style={emailStyle}>{store.user?.logged_in_as || "Cargando información del usuario..."}</p>
+		<div className="containerStyle">
+			<h3 className="headingStyle">Usuario autenticado:</h3>
+			<p  className="emailStyle">{store.user?.logged_in_as || "Cargando información del usuario..."}</p>
 		</div>
 	);
 };
